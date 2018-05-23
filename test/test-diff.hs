a4e43f92ace35c83ca7aac3c1a4f6e1e6e695ff6
Lysxia <lysxia@gmail.com>
2018-05-23 16:25:48 -0400

    Improve test-diff test case generation

--- test/test-diff.hs
+++ test/test-diff.hs
@@ -1,4 +1,11 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns #-}
+
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 import Data.Bifunctor (bimap)
+import Data.Coerce
 import Data.Algorithm.Diff
   ( Diff(..), getGroupedDiff )
 
@@ -9,8 +16,8 @@ import Test.Tasty.Diff.Internal.Diff
 
 type X = Int
 
-prop_getGroupedDiff :: [X] -> [X] -> Bool
-prop_getGroupedDiff xs ys = goodGroupedDiff ds where
+prop_getGroupedDiff :: [X] -> [X] -> Property
+prop_getGroupedDiff xs ys = interestingDiff ds ==> goodGroupedDiff ds where
   ds = getGroupedDiff xs ys
   -- -- To check statistics
   -- label prop = label (mkLabel ds)
@@ -34,10 +41,14 @@ prop_correct_contextDiff (CtxLen_ ctxLen) xs ys = correctDiff xs ys gs0 where
 
 main :: IO ()
 main = defaultMain $ testGroup "Test"
-  [ testProperty "correct_getGroupedDiff" prop_correct_getGroupedDiff
-  , testProperty "getGroupedDiff" prop_getGroupedDiff
-  , testProperty "correct_contextDiff" prop_correct_contextDiff
-  , testProperty "contextDiff" prop_contextDiff
+  [ testProperty "correct_getGroupedDiff" $ \(DiffList xs ys) ->
+      prop_correct_getGroupedDiff xs ys
+  , testProperty "getGroupedDiff" $ \(DiffList xs ys) ->
+      prop_getGroupedDiff xs ys
+  , testProperty "correct_contextDiff" $ \ctxLen_ (DiffList xs ys) ->
+      prop_correct_contextDiff ctxLen_ xs ys
+  , testProperty "contextDiff" $ \ctxLen_ (DiffList xs ys) ->
+      prop_contextDiff ctxLen_ xs ys
   ]
 
 goodGroupedDiff :: Eq t => [Diff [t]] -> Bool
@@ -79,3 +90,34 @@ extractDiff = bimap concat concat . unzip . fmap fromDiff where
 
 correctDiff :: (Eq t, Show t) => [t] -> [t] -> [Diff [t]] -> Property
 correctDiff xs ys ds = extractDiff ds === (xs, ys)
+
+interestingDiff :: [Diff [t]] -> Bool
+interestingDiff ds = or [ length a > 1 | Both a _ <- ds ]
+
+instance Arbitrary a => Arbitrary (Diff a) where
+  arbitrary = elements [First, Second, both] <*> arbitrary
+  shrink (First as) = both as : fmap First (shrink as)
+  shrink (Second as) = both as : fmap Second (shrink as)
+  shrink (Both as _) = fmap both (shrink as)
+
+strangeList :: Gen a -> Gen [a]
+strangeList g = sized $ \n -> do
+  x <- choose (1, sqrt (fromIntegral n + 4) :: Double)
+  let i = floor (x * x)
+  vectorOf i g
+
+newtype StrangeList a = StrangeList [a]
+
+instance Arbitrary a => Arbitrary (StrangeList a) where
+  arbitrary = StrangeList <$> strangeList arbitrary
+  shrink (StrangeList as) = StrangeList <$> shrink as
+
+newtype StrangeDiff a = StrangeDiff [Diff [a]]
+  deriving Show
+
+pattern DiffList :: [a] -> [a] -> StrangeDiff a
+pattern DiffList xs ys <- StrangeDiff (extractDiff -> (xs, ys))
+
+instance Arbitrary a => Arbitrary (StrangeDiff a) where
+  arbitrary = coerce (arbitrary :: Gen [Diff (StrangeList a)])
+  shrink (StrangeDiff ds) = StrangeDiff <$> shrink ds
